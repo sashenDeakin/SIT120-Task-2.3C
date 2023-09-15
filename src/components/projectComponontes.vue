b86462fd5592b256e3fd15354b308b0b4126b530
Sashen Jayathilaka <139418339+sashenDeakin@users.noreply.github.com>
2023-09-15 13:32:08 +1000

    SIT120-Task-2.3C

---
+++ src/components/projectComponontes.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>This is from project Components</div>
+
+  <br />
+  Title {{ title }}
+
+  <br />
+
+  Id: {{ id }}
+
+  <br />
+
+  Description
+  <p>{{ body }}</p>
+  <button @click="$emit('increase-size')">Increase Front</button>
+  <button>Button with slot</button>
+  <button><slot></slot></button>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  id: Number,
+  body: String,
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,53 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from "vue";
+import ProjectComponent from "../components/projectComponontes.vue";
+
+const posts = ref([]);
+
+const fetchPost = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+
+    posts.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(fetchPost);
+
+const postFrontSize = ref(1);
+
+function increaseSize() {
+  postFrontSize.value += 1;
+}
+
+import datafld from "../data/data.json";
+const datalist = ref();
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div :style="{ fontSize: postFrontSize + 'em' }">
+    <div>
+      <p>Post Page</p>
+    </div>
+    <!--   <ol>
+    <li v-for="post in posts">{{ post.title }}</li>
+  </ol> -->
+
+    <div>
+      <ProjectComponent
+        v-for="post in posts"
+        :key="post.id"
+        :id="post.id"
+        :title="post.title"
+        :body="post.body"
+        @increase-size="increaseSize"
+        ><!-- Button WIth Slot Two -->
+        {{ post.id }}
+        <template #footer> This is Name SLot </template>
+      </ProjectComponent>
+    </div>
+  </div>
 </template>
